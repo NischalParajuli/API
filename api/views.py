9f77ca40ac60f41451838a091a0aefa53937c6c2
NischalParajuli <parajulinischal204@gmail.com>
2026-03-27 09:35:32 +0545

    Added delete amd post features

--- api/views.py
+++ api/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-# Create your views here.
+from .models import *
+from .serializer import CategorySerializer
 
-@api_view()
+
+@api_view(['GET' ,'POST'])
 def categorylist(request):
-   return Response("ok")
+    if request.method == "GET":
+        category = Category.objects.all()
+        serializer = CategorySerializer(category,many = True) # converts to    json 
+        return Response(serializer.data)
+    elif request == "POST":
+        serializer = CategorySerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+
+@api_view(['GET','DELETE'])
+def categoryDetail(request,id):
+    category = Category.objects.get(id = id)
+    if request.method == "GET":
+        serializer = CategorySerializer(category) 
+        return Response(serializer.data)
+    elif request.method == "DELETE":
+        items = OrderItem.objects.filter(food__category = category).count()
+        if items > 0 :
+            return Response({"detail":"protected error : Data cannot be deleted"})
+        category.delete()
+        return Response({"detail": "Data has been deleted."})
